dbbc19373fa6a1a17b6c003276fb883029fb99b4
Ryan Rasmuss <ryan.rasmuss@1touch.io>
2022-05-05 16:08:24 -0700

    working upload and download function - not actually downloading pdf

--- python/README.md
+++ python/README.md
@@ -152,3 +152,125 @@
 
 # Verdict Response MALICIOUS
 
+```json
+{
+  "response": [
+    {
+      "status": {
+        "code": 1001,
+        "label": "FOUND",
+        "message": "The request has been fully answered."
+      },
+      "sha1": "586b5a65430263f62d656c96624967122568e274",
+      "md5": "eefa6f98681d78b63f15d7e58934c6cc",
+      "sha256": "653bc2b16b1624e045c1225810185e9aa3694dc378fe0095e2052b7f1e265d01",
+      "file_type": "exe",
+      "file_name": "Win32.WannaPeace.exe",
+      "features": [
+        "av",
+        "te"
+      ],
+      "av": {
+        "malware_info": {
+          "signature_name": "Trojan.Win32.Ransomware.Win32.Wannapeace.TC.a",
+          "malware_family": 312510,
+          "malware_type": 114,
+          "severity": 4,
+          "confidence": 5
+        },
+        "status": {
+          "code": 1001,
+          "label": "FOUND",
+          "message": "The request has been fully answered."
+        }
+      },
+      "te": {
+        "trust": 10,
+        "images": [
+          {
+            "report": {
+              "verdict": "malicious"
+            },
+            "status": "found",
+            "id": "e50e99f3-5963-4573-af9e-e3f4750b55e2",
+            "revision": 1
+          },
+          {
+            "report": {
+              "verdict": "malicious"
+            },
+            "status": "found",
+            "id": "3ff3ddae-e7fd-4969-818c-d5f1a2be336d",
+            "revision": 1
+          }
+        ],
+        "score": -2147483648,
+        "combined_verdict": "malicious",
+        "confidence": 3,
+        "severity": 4,
+        "status": {
+          "code": 1001,
+          "label": "FOUND",
+          "message": "The request has been fully answered."
+        }
+      }
+    }
+  ]
+}
+```
+
+# RESPONSE MALICIOUS WHERE IS PDF REPORT ID? HERE!
+```json
+200
+{
+  "response": {
+    "status": {
+      "code": 1001,
+      "label": "FOUND",
+      "message": "The request has been fully answered."
+    },
+    "sha1": "586b5a65430263f62d656c96624967122568e274",
+    "md5": "eefa6f98681d78b63f15d7e58934c6cc",
+    "sha256": "653bc2b16b1624e045c1225810185e9aa3694dc378fe0095e2052b7f1e265d01",
+    "file_type": "",
+    "file_name": "Win32.WannaPeace.exe",
+    "features": [
+      "te"
+    ],
+    "te": {
+      "trust": 10,
+      "images": [
+        {
+          "report": {
+            "verdict": "malicious",
+            "pdf_report": "67dcf393-18fa-4118-93c6-483720ba33af",
+            "xml_report": "4f18319e-99da-43d5-bbdd-af51b791f58f"
+          },
+          "status": "found",
+          "id": "3ff3ddae-e7fd-4969-818c-d5f1a2be336d",
+          "revision": 1
+        },
+        {
+          "report": {
+            "verdict": "malicious",
+            "pdf_report": "6942ca3f-61fc-49bc-a24f-54f34bc14530",
+            "xml_report": "7b162f59-ac38-4113-99d3-49d5bab3a4c8"
+          },
+          "status": "found",
+          "id": "e50e99f3-5963-4573-af9e-e3f4750b55e2",
+          "revision": 1
+        }
+      ],
+      "score": -2147483648,
+      "combined_verdict": "malicious",
+      "severity": 4,
+      "confidence": 3,
+      "status": {
+        "code": 1001,
+        "label": "FOUND",
+        "message": "The request has been fully answered."
+      }
+    }
+  }
+}
+```
--- python/threatcloud.py
+++ python/threatcloud.py
@@ -5,134 +5,155 @@ import json
 import sys
 
 def getKey():
-	keyfile = '.threatcloud.key'
-	key = ''
+    keyfile = '.threatcloud.key'
+    key = ''
 
-	# potential error: FileNotFoundError: [Errno 2] No such file or directory: '.threatcloud.key'
-	with open(keyfile, 'r') as f:
-		key = f.read().rstrip('\n')
+    # potential error: FileNotFoundError: [Errno 2] No such file or directory: '.threatcloud.key'
+    with open(keyfile, 'r') as f:
+        key = f.read().rstrip('\n')
 
-	f.close()
+    f.close()
 
-	return key
+    return key
 
 
 def post(address, command, payload):
 
-	address = address + command
-	print("POST: " + address)
-	return None
+    address = address + command
+    print("POST: " + address)
+    return None
 
-def upload(address):
+def upload(address, filename):
 
-	# will need to be an arg
-	filename = "LoaderDemo4.xlsm"
+    # will need to be an arg
+    #filename = "LoaderDemo4.xlsm"
 
-	address = address + 'upload'
+    address = address + 'upload'
 
-	request = {"request": { "file_name": filename,
-		"te": { "reports": ["pdf", "xml"]} }}
+    request = {"request": { "file_name": filename,
+        "te": { "reports": ["pdf", "xml"]} }}
 
-	files = { "file": (open(filename, "rb")), 'request': json.dumps(request)}
-	# payload = { "request": { "features": ["te"], "te": {"reports": ["pdf"] }}}
-	#payload = { "request": { "features": ["te"], "te": {"reports": ["pdf"] }} }
+    files = { "file": (open(filename, "rb")), 'request': json.dumps(request)}
+    # payload = { "request": { "features": ["te"], "te": {"reports": ["pdf"] }}}
+    #payload = { "request": { "features": ["te"], "te": {"reports": ["pdf"] }} }
 
 
 
-	key = getKey()
-	# request_headers = { 'Authorization': key, 'Content-Type': 'multipart/form-data'}
-	request_headers = { 'Authorization': key } 
-	
-	print("POST %s" % address)
+    key = getKey()
+    # request_headers = { 'Authorization': key, 'Content-Type': 'multipart/form-data'}
+    request_headers = { 'Authorization': key } 
+    
+    print("POST %s" % address)
 #print("headers: %s " % request_headers)
-	#print("Payload: %s " % payload)
+    #print("Payload: %s " % payload)
 
-	#r = requests.post(address, headers=request_headers, data=payload, files=files)
-	r = requests.post(address, headers=request_headers, files=files)
-	print(r.status_code)
-	print(r.text)
+    #r = requests.post(address, headers=request_headers, data=payload, files=files)
+    r = requests.post(address, headers=request_headers, files=files)
+    print(r.status_code)
+    print(r.text)
+
+
+def download(address, fileid):
+
+    address = address + 'download?id=' + fileid
+
+    print("Sending download request to: %s" % address)
+
+    params = { "id": fileid }
+    r = requests.get(address, params=params)
+
+    print(r.status_code)
+    filename = fileid + ".pdf"
+
+    with open(filename, "wb") as report:
+        report.write(r.content)
 
 
 def query(address, filename):
 
-	address = address + 'query'
-	filehash = hashlib.md5(open(filename, 'rb').read()).hexdigest()
-	print("%s hash %s" % (filename, filehash))
+    address = address + 'query'
+    filehash = hashlib.md5(open(filename, 'rb').read()).hexdigest()
+    print("%s hash %s" % (filename, filehash))
 
-	key = getKey()
-	request_headers = { 'Authorization': key }
-	requestnot = { "file_name": filename, "md5": filehash, "features": ["av", "te"] }
-	request = { "request": [ requestnot ]}
+    key = getKey()
+    request_headers = { 'Authorization': key }
+    requestnot = { "file_name": filename, "md5": filehash, "features": ["av", "te"] }
+    request = { "request": [ requestnot ]}
 
-	r = requests.post(address, data=json.dumps(request), headers=request_headers)
-	print(r.status_code)
-	#print(r.text)
-	pretty = json.dumps(r.json(), indent=2)
-	#print(pretty)
-
-
-	# Turn response into dictionary
-	response = json.loads(r.text)
-	# bad response formatting from service... fixing it
-	response = response["response"]
-	# most likely because you can query multiple file_names at once? Deserialize ...
-	response = response[0]
-	#print(type(response))
-	#print(response)
-	#print (type(response))
-
-	#print (response.keys())
-
-	av_response = response["av"]["status"]["label"]
-	print("AV verdict: %s" % av_response)
-	combined_verdit = response["te"]["combined_verdict"]
-	print("Emulation verdict: %s" % combined_verdit)
+    r = requests.post(address, data=json.dumps(request), headers=request_headers)
+    print(r.status_code)
+    #print(r.text)
+    pretty = json.dumps(r.json(), indent=2)
+    print(pretty)
 
 
-	
+    # Turn response into dictionary
+    response = json.loads(r.text)
+    # bad response formatting from service... fixing it
+    response = response["response"]
+    # most likely because you can query multiple file_names at once? Deserialize ...
+    response = response[0]
+    #print(type(response))
+    #print(response)
+    #print (type(response))
+
+    #print (response.keys())
+
+#   av_response = response["av"]["status"]["label"]
+#   print("AV verdict: %s" % av_response)
+#   combined_verdict = response["te"]["combined_verdict"]
+#   print("Emulation verdict: %s" % combined_verdict)
+
+
+    
 
 def quota(address):
 
 
-	address = address + 'quota'
-	print("POST: " + address)
-	key = getKey()
-	request_headers = { 'Authorization': key }
-	r = requests.post(address, headers=request_headers)
-	print(r.status_code)
-	print(r.text)
+    address = address + 'quota'
+    print("POST: " + address)
+    key = getKey()
+    request_headers = { 'Authorization': key }
+    r = requests.post(address, headers=request_headers)
+    print(r.status_code)
+    print(r.text)
 
 
 def main():
 
-	args = len(sys.argv)
+    args = len(sys.argv)
 
-	if args > 3 or args < 2:
-		print("Usage: python3 threatcloud.py query|upload|quota file")
-		return None
+    if args > 3 or args < 2:
+        print("Usage: python3 threatcloud.py query|upload|quota file")
+        return None
 
-	address = 'https://te.checkpoint.com/tecloud/api/v1/file/'
-	
-	#quota(address)
-	#post(address, 'query', None)
-	#post(address, 'upload', None)
-	#post(address, 'download', None)
+    address = 'https://te.checkpoint.com/tecloud/api/v1/file/'
+    
+    #quota(address)
+    #post(address, 'query', None)
+    #post(address, 'upload', None)
+    #post(address, 'download', None)
 
-	#upload_av(address)
+    #upload_av(address)
 
-	if args == 2 and sys.argv[1] == 'quota':
-		quota(address)
-		return None
+    if args == 2 and sys.argv[1] == 'quota':
+        quota(address)
+        return None
 
-	command, filename = sys.argv[1], sys.argv[2]
+    command, filename = sys.argv[1], sys.argv[2]
 
-	if command == 'query':
-		query(address, filename)
-	if command == 'upload':
-		upload(address, filename)
-	
+    if command == 'query':
+        query(address, filename)
+    if command == 'upload':
+        upload(address, filename)
+
+    if command == 'download':
+
+        download(address, filename)
+        # filename is actually the ID Lol
+    
 
 
 if __name__ == '__main__':
 
-	main()
+    main()
